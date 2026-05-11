24049268e0dcfc7058ef41bcc5c6be92bf6e68eb
gonzalestrisha <21-08571@g.batstate-u.edu.ph>
2026-05-11 22:29:20 +0800

    Add initial project files and .gitignore for MBE-python

---
+++ MBE-files/oil-reservoir.py
@@ -0,0 +1,16 @@
+# Oil Reservoir Files
+
+def oilVolumetricCalc():
+  pass
+
+def oilReservoirMBEwithInjection():
+  pass
+
+def oilReservoirMBEnoInjection():
+  pass
+
+def oilMBEasLinearFunction():
+  pass
+
+def expressionOfMBEasTwoPhaseFormation():
+  
